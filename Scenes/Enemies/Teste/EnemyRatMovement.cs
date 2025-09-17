5b3e2da7df57387c439264f310160a840496eca1
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-09-16 22:17:39 -0300

    Adiciona lógica de patrulha e perseguição para inimigos
    - Atualiza `EnemieTeste.tscn` com novos nós (`RayCast2D`, `Markers`)
      e propriedades para suporte à patrulha e perseguição.
    - Introduz enum `State` no script `EnemyRatMovement.cs` para gerenciar
      estados (`Idle`, `Chasing`, `Attacking`, `Returning`).
    - Implementa lógica de patrulha com alternância entre marcadores.
    - Adiciona métodos para iniciar e parar perseguição ao jogador.
    - Substitui navegação baseada em `NavigationAgent2D` por movimentação
      direta com vetores.
    - Atualiza `Phase01.tscn` com marcadores de patrulha e configurações
      adicionais para inimigos.
    - Ajusta comportamento de colisão com o jogador usando `RayCast2D`.

--- Scenes/Enemies/Teste/EnemyRatMovement.cs
+++ Scenes/Enemies/Teste/EnemyRatMovement.cs
@@ -5,46 +5,82 @@ using System.Linq;
 
 public partial class EnemyRatMovement : CharacterBody2D
 {
+    private enum State
+    {
+        Idle,
+        Chasing,
+        Attacking,
+        Returning
+    }
+
+
     [Export]
-    public int SPEED { get; set; } = 100;
+    public int SPEED { get; set; } = 500;
 
     [Export]
     private NavigationAgent2D Agent { get; set; }
 
+    [Export]
+    private RayCast2D RayCast { get; set; }
+
+    [Export]
+    private Timer Timer { get; set; }
+
     private GameManager GameManagerInstance { get; set; }
+
+    private State CurrentState { get; set; } = State.Idle;
+
+    [Export]
+    private Marker2D[] PositionsToPatrol { get; set; } = [];
+
+    private Marker2D CurrentPatrolPosition { get; set; }
 
     public override void _Ready()
     {
         GameManagerInstance = GameManager.GetInstance();
-        GoToTarget();
+        if (PositionsToPatrol.Length > 0)
+            CurrentPatrolPosition = PositionsToPatrol[0];
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        if (Agent.IsNavigationFinished())
-        {
-            Velocity = Vector2.Zero;
-            return;
-        }
-        GD.Print($"Agent path point count: {Agent.GetCurrentNavigationPath().Count()}");
+        if (this.GlobalPosition.DistanceTo(CurrentPatrolPosition.Position) < 10)
+            GetNextPosition();
 
-        Vector2 nextPoint = Agent.GetNextPathPosition().Normalized();
-        Velocity = nextPoint * (float)delta * SPEED;
+        if(CurrentState == State.Idle || CurrentState == State.Returning)
+        {
+            Vector2 direction = (CurrentPatrolPosition.Position - this.GlobalPosition);
+            Velocity = direction * SPEED * (float) delta;
+        }
+
+
+        if (RayCast.IsColliding() && RayCast.GetCollider() is BasePlayer player && this.CurrentState != State.Chasing)
+            ChasePlayer();
+        else if (!RayCast.IsColliding() && this.CurrentState == State.Chasing)
+            StopChase();
+
         MoveAndSlide();
     }
 
-    private void GoToTarget()
+    private void GetNextPosition()
     {
-        var (activePlayer, _) = GameManagerInstance.GetActiveAndInactivePlayer();
-        if (activePlayer != null)
-        {
-            var targetPos = activePlayer.GetCurrentPosition();
-            Agent.SetTargetPosition(targetPos);
-        }
+        CurrentPatrolPosition = PositionsToPatrol[(Array.IndexOf(PositionsToPatrol, CurrentPatrolPosition) + 1) % PositionsToPatrol.Length];
     }
 
+    private void StopChase()
+    {
+        this.CurrentState = State.Returning;
+    }
+
+    private void ChasePlayer()
+    {
+        this.CurrentState = State.Chasing;
+        SPEED = 250;
+    }
+
+  
     public void OnNavTimer_timeout()
     {
-        GoToTarget();
+        
     }
 }
